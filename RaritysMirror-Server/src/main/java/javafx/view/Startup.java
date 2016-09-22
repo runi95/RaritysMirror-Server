b90f4ef0cbace59b8846bfe88de9a605a0cb01bc
Night <thegreatandpowerfulmetal@gmail.com>
2016-09-22 17:26:09 +0200

    Added dummy events

--- RaritysMirror-Server/src/main/java/javafx/view/Startup.java
+++ RaritysMirror-Server/src/main/java/javafx/view/Startup.java
@@ -4,8 +4,30 @@ import javafx.Resources;
 import javafx.scene.control.TitledPane;
 
 public class Startup extends TitledPane {
-
+	
 	public Startup() {
 		Resources.loadFXML(this);
+	}
+	
+	//On actions:
+	
+	public void openEditorClicked() {
+		//TODO: Add stuffs
+		System.out.println("open editor clicked!");
+	}
+	
+	public void loadClicked() {
+		//TODO: Add stuffs
+		System.out.println("load button clicked!");
+	}
+	
+	public void localhostChecked() {
+		//TODO: Add stuffs
+		System.out.println("localhost changed!");
+	}
+	
+	public void startServerClicked() {
+		//TODO: Add stuffs
+		System.out.println("start server clicked!");
 	}
 }

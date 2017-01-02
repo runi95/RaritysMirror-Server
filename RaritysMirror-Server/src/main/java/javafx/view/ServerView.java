26f47980db593083c6b78425cf7486fe5ed04648
Night <thegreatandpowerfulmetal@gmail.com>
2017-01-02 01:40:10 +0100

    Unused methods removed

--- RaritysMirror-Server/src/main/java/javafx/view/ServerView.java
+++ RaritysMirror-Server/src/main/java/javafx/view/ServerView.java
@@ -6,21 +6,6 @@ public class ServerView extends BorderPane {
 	
 	//On actions:
 	
-	public void openEditorClicked() {
-		//TODO: Add stuffs
-		System.out.println("open editor clicked!");
-	}
-	
-	public void loadClicked() {
-		//TODO: Add stuffs
-		System.out.println("load button clicked!");
-	}
-	
-	public void localhostChecked() {
-		//TODO: Add stuffs
-		System.out.println("localhost changed!");
-	}
-	
 	public void startServerClicked() {
 		//TODO: Add stuffs
 		System.out.println("start server clicked!");
